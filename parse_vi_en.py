5885e02e39145d246facd6325fce0f26eb59abf9
png261 <nhphuong.code@gmail.com>
2023-10-18 15:30:11 +0700

    feat: change db schema

---
+++ parse_vi_en.py
@@ -0,0 +1,66 @@
+import sqlite3
+import json
+
+
+def create_database(database, schemaFile):
+    with open(schemaFile, 'r') as sql_file:
+        sql_script = sql_file.read()
+
+    cursor = database.cursor()
+    cursor.executescript(sql_script)
+    database.commit()
+
+
+def fix_quote(string):
+    return string.replace("'", "''")
+
+
+def query_word(word, pronounce):
+    word = fix_quote(word)
+    pronounce = fix_quote(pronounce)
+    return f"INSERT INTO words(word, pronounce) VALUES('{word}', '{pronounce}')"
+
+
+def query_explain(word_id, explain_type, meaning):
+    explain_type = fix_quote(explain_type)
+    meaning = fix_quote(meaning)
+    return f"INSERT INTO explains(word_id, type, meaning) VALUES('{word_id}','{explain_type}', '{meaning}')"
+
+
+def query_example(explain_id, example, translate):
+    example = fix_quote(example)
+    translate = fix_quote(translate)
+    return f"INSERT INTO examples(explain_id, example, translate) VALUES('{explain_id}','{example}', '{translate}')"
+
+
+def parse_json(database, jsonFile):
+    with open(jsonFile, 'r') as json_file:
+        data = json.load(json_file)
+        cursor = database.cursor()
+
+        for word, value in data.items():
+            cursor.execute(query_word(word, value["pronoun"]))
+            word_id = cursor.lastrowid
+
+            for explain in value["type"]:
+                for explain_type, explain_value in explain.items():
+                    if not explain_value:
+                        break
+
+                    for meaning, examples in explain_value[0].items():
+                        cursor.execute(query_explain(
+                            word_id, explain_type, meaning))
+                        explain_id = cursor.lastrowid
+
+                        for example in examples:
+                            for ex, trans in example.items():
+                                cursor.execute(query_example(
+                                    explain_id, ex, trans))
+    database.commit()
+
+
+database = sqlite3.connect('database/vi-en.db')
+create_database(database, 'sql/schema.sql')
+parse_json(database, 'json/vietnamese-english.json')
+database.commit()
+database.close()
--- sql/schema.sql
+++ sql/schema.sql
@@ -12,28 +12,14 @@ CREATE TABLE words (
 
 CREATE TABLE explains (
     explain_id INTEGER NOT NULL PRIMARY KEY,
+    word_id INTEGER NOT NULL,
     type VARCHAR(255),
     meaning VARCHAR(255)
 );
 
 CREATE TABLE examples (
     example_id INTEGER NOT NULL PRIMARY KEY,
+    explain_id INTEGER NOT NULL,
     example VARCHAR(255),
     translate VARCHAR(255)
-);
-
-CREATE TABLE words_explains (
-    words_explains_id INTEGER NOT NULL PRIMARY KEY,
-    word_id INTEGER NOT NULL,
-    explain_id INTEGER NOT NULL,
-    FOREIGN KEY (word_id) REFERENCES words(word_id),
-    FOREIGN KEY (explain_id) REFERENCES explains(explain_id)
-);
-
-CREATE TABLE explains_examples (
-    explains_examples_id INTEGER NOT NULL PRIMARY KEY,
-    explain_id INTEGER NOT NULL,
-    example_id INTEGER NOT NULL,
-    FOREIGN KEY (explain_id) REFERENCES explains(explain_id),
-    FOREIGN KEY (example_id) REFERENCES examples(example_id)
 );
